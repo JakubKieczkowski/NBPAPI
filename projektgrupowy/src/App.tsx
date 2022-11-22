971adfe5363235575473ec086336a76698f3dd2a
Jakub Kięczkowski <88kkieczkowski@gmail.com>
2022-11-22 19:20:08 +0100

    Jakub K. 22.11.2022 19:16

--- projektgrupowy/src/App.tsx
+++ projektgrupowy/src/App.tsx
@@ -1,12 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
 import HomePage from './components/HomePage/HomePage';
+import RegisterPage from './components/RegisterPage/RegisterPage';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import LoginPage from './components/LoginPage/LoginPage';
+import { onAuthStateChanged } from 'firebase/auth';
+import { auth } from './firebaseConfig';
+import { authContext } from './helpers/authContext';
 
 function App() {
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      setLoggedIn(true);
+    } else {
+      setLoggedIn(false);
+    }
+  });
+
   return (
     <div className="App">
-     <HomePage />
+      <BrowserRouter>
+        <authContext.Provider value={loggedIn}>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/register" element={<RegisterPage />} />
+            <Route path="/login" element={<LoginPage />} />
+          </Routes>
+        </authContext.Provider>
+      </BrowserRouter>
     </div>
   );
 }
