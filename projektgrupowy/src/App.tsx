d51f626684f0f897f497a1588f86084960c46f6d
kat war <natasza.warmuz@gmail.com>
2022-11-22 19:54:43 +0100

    currency_page

--- projektgrupowy/src/App.tsx
+++ projektgrupowy/src/App.tsx
@@ -1,12 +1,13 @@
-import { useState } from 'react';
-import Navbar from './components/Navbar/Navbar';
-import HomePage from './components/HomePage/HomePage';
-import RegisterPage from './components/RegisterPage/RegisterPage';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import LoginPage from './components/LoginPage/LoginPage';
-import { onAuthStateChanged } from 'firebase/auth';
-import { auth } from './firebaseConfig';
-import { authContext } from './helpers/authContext';
+import { useState } from "react";
+import Navbar from "./components/Navbar/Navbar";
+import HomePage from "./components/HomePage/HomePage";
+import RegisterPage from "./components/RegisterPage/RegisterPage";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LoginPage from "./components/LoginPage/LoginPage";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "./firebaseConfig";
+import { authContext } from "./helpers/authContext";
+import CurrencyConverter from "./components/CurrencyPage/CurrencyPage";
 
 function App() {
   const [loggedIn, setLoggedIn] = useState(false);
@@ -28,6 +29,7 @@ function App() {
             <Route path="/" element={<HomePage />} />
             <Route path="/register" element={<RegisterPage />} />
             <Route path="/login" element={<LoginPage />} />
+            <Route path="/currency" element={<CurrencyConverter />} />
           </Routes>
         </authContext.Provider>
       </BrowserRouter>
