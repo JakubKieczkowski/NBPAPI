8cb65c86fbb911d3a6acef04e7997a1b1e06ed8f
PaulinaS <smigielska.p@gmail.com>
2022-11-22 19:10:07 +0100

    add navbar 1.0

--- projektgrupowy/src/App.tsx
+++ projektgrupowy/src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar/Navbar';
 import HomePage from './components/HomePage/HomePage';
 
 function App() {
   return (
     <div className="App">
      <HomePage />
+     <Navbar />
     </div>
   );
 }
