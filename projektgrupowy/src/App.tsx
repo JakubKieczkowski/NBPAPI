013fbba161d25ec3c04531fe9867a2469e7d7147
kat war <natasza.warmuz@gmail.com>
2022-11-22 18:30:51 +0100

    22.11:18:30

--- projektgrupowy/src/App.tsx
+++ projektgrupowy/src/App.tsx
@@ -1,12 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import HomePage from './components/HomePage/HomePage';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import HomePage from "./components/HomePage/HomePage";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import CurrencyConverter from "./components/CurrencyPage/CurrencyPage";
 
 function App() {
   return (
     <div className="App">
-     <HomePage />
+      <HomePage />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />}></Route>
+          {/* <Route path="/register" element={<RegisterPage />} /> */}
+          {/* <Route path="/login" element={<LoginPage />} /> */}
+          <Route path="/converter" element={<CurrencyConverter />} />
+        </Routes>
+        {/* ---- */}
+        {/* tu poniżej znow kompo, ktore maja sie wyswietlac caly czas - np. footer  */}
+      </BrowserRouter>
     </div>
   );
 }
